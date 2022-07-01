ccee5a0f4e913e1bd4455236ae56c7c4c21f85e1
Abakar Ramazanov <abakar.r.ramazanov@yandex.ru>
2022-07-01 09:21:49 +0300

    fix compilation

--- ch01/1.4/main.cpp
+++ ch01/1.4/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-    int a, bl;
+    int a, b;
     std::cout << "Insert two integers:" << std::endl;
-    std::cin >> a >> b;
+    // std::cin >> a >> b;
     std::cout << "a * b = " << a << " * " << b << " = " << a * b << std::endl;
     return 0;
 }
